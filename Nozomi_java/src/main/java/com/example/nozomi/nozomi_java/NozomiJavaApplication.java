454217e63e8c5e51d64c33f542eaa1c33434b1c1
ALeoNya <2923509522@qq.com>
2024-04-03 08:53:32 +0800

    to main

---
+++ Nozomi_java/src/main/java/com/example/nozomi/nozomi_java/NozomiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nozomi.nozomi_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NozomiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NozomiJavaApplication.class, args);
+    }
+
+}
